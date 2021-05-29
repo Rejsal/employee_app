e60f79e132d9f12fd4eac41360b09b73b8d89a54
Admin <admin@Admins-MBP.domain.name>
2021-05-29 19:35:30 +0530

    image view updated

--- src/screens/employee/view/index.js
+++ src/screens/employee/view/index.js
@@ -32,9 +32,11 @@ function ViewEmployee() {
                     </TouchableOpacity>
                 <Text style={styles.heading}>{employee.name ? employee.name : ''}</Text>               
                 </View>
-                <View style={{marginTop:20}}>
+                <View style={{marginTop:25}}>
+                {employee.profile_image ? <Image style={styles.imageContainer} source={{ uri: employee.profile_image }} /> : <View style={[styles.imageContainer, styles.alignCenter]}><Image style={styles.icon} source={require('../../../assets/error.png')} /></View>}
                 {employee.email?<Text style={styles.contentTitle}>Email: <Text style={styles.content}>{employee.email}</Text></Text>:null}
                 {employee.username?<Text style={styles.contentTitle}>Username: <Text style={styles.content}>{employee.username}</Text></Text>:null}
+                {employee.phone?<Text style={styles.contentTitle}>Phone: <Text style={styles.content}>{employee.phone}</Text></Text>:null}
                 {employee.website?<Text style={styles.contentTitle}>Website: <Text style={styles.content}>{employee.website}</Text></Text>:null}
                 {employee.address?<Text style={styles.contentTitle}>Address: <Text style={styles.content}>{`${employee.address.street}, ${employee.address.city?employee.address.city:''}`}</Text></Text>:null}
                 {employee.company?<Text style={styles.contentTitle}>Company Details: <Text style={styles.content}>{`${employee.company.name}, ${employee.company.bs?employee.company.bs:''}`}</Text></Text>:null}
@@ -54,6 +56,18 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff'
     },
+    imageContainer: {
+        width: 60,
+        height: 60,
+        borderRadius: 6,
+        backgroundColor: 'grey'
+    },
+    icon: {
+        width: 20,
+        height: 20,
+        resizeMode: 'contain',
+        tintColor: '#fff'
+    },
     titleView: {
         alignItems: 'center',
         flexDirection: 'row'
@@ -62,6 +76,10 @@ const styles = StyleSheet.create({
         height: 20,
         width:20,
         resizeMode:'contain'
+    },
+    alignCenter: {
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     listHeader: {
         height: 16
@@ -79,7 +97,7 @@ const styles = StyleSheet.create({
     contentTitle: {
         color: 'grey',
         fontSize: 16,
-        marginTop: 2
+        marginTop: 10
     },
     content: {
         color: '#000000',
